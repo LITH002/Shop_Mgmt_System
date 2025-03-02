8d479a2bc27ee397c45da9252eb2707f57800643
LITH002 <liteenilanjith@gmail.com>
2025-03-02 16:30:25 +0530

    Cart icon was added to the assets folder

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -2,12 +2,14 @@ import logo from './CF_Logo.jpeg';
 import font from './CF_Font.png';
 import cake_logo from './Cake_Logo.png';
 import search_icon from './Search_Icon.png';
+import cart from './Cart.png';
 
 const assets = {
     logo,
     font,
     cake_logo,
     search_icon,
+    cart,
 };
 
 export default assets;
