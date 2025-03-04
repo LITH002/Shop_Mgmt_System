b789769afd880d30d9b2ea68cb3df1103f499d2c
LITH002 <liteenilanjith@gmail.com>
2025-03-04 17:53:18 +0530

    Complete Navigation Bar & creation of the Cart folder

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
 //import React from 'react'
 import './Navbar.css'
 import assets from '../../assets/assets.js'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("Category");
   return (
     <div className='navbar'>
       <img src={assets.font} alt="" className="text" />
       <ul className="navbar-menu">
-        <li>Category</li>
-        <li>Contact Us</li>
+        <li onClick={()=>setMenu("Category")} className={menu==="Category"?"active":""}>Category</li >
+        <li onClick={()=>setMenu("Contact Us")} className={menu==="Contact Us"?"active":""}>Contact Us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
-        <div className='navbar-search-icon'></div>
+        <div className='navbar-search-icon'>
+          <img src={assets.cart} className='cart-icon' alt="" />
+          <div className="dot"></div>
+        </div>
         <button>Place Order</button>
       </div>
     </div>
