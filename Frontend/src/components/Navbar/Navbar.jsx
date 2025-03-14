f24753959305dbf9eca34acf7dbe188f3dfff238
LITH002 <liteenilanjith@gmail.com>
2025-03-14 18:48:41 +0530

    Navbar Component updated

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,30 @@
-//import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
-import assets from '../../assets/assets.js'
-import { useState } from 'react'
+import assets from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
-const Navbar = () => {
-  const [menu, setMenu] = useState("Category");
+const Navbar = ({setShowLogin}) => {
+
+  const [menu, setMenu] = useState("menu");
+  
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
     <div className='navbar'>
-      <img src={assets.font} alt="" className="text" />
+      <Link to='/'><img src={assets.font} alt="" className="text" /></Link>
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("Category")} className={menu==="Category"?"active":""}>Category</li >
-        <li onClick={()=>setMenu("Contact Us")} className={menu==="Contact Us"?"active":""}>Contact Us</li>
+        <Link to='/' onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link >
+        <a href='#explore-categories' onClick={()=>setMenu("Items")} className={menu==="Items"?"active":""}>Items</a>
+        <a href='#footer' onClick={()=>setMenu("Contact Us")} className={menu==="Contact Us"?"active":""}>Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className='navbar-search-icon'>
-          <img src={assets.cart} className='cart-icon' alt="" />
-          <div className="dot"></div>
+        <Link to='/cart'><img src={assets.cart} className='cart-icon' alt="" /></Link>
+          <div className={getTotalCartAmount()===0?"0":"dot"}></div>
         </div>
-        <button>Place Order</button>
+        <button onClick={() => setShowLogin(true)}>Place Order</button>
       </div>
     </div>
   )
