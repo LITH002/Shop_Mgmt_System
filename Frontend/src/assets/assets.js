acfa140049ad0f94fc4969ccb0a686a4933976bd
LITH002 <liteenilanjith@gmail.com>
2025-03-15 12:22:40 +0530

    Assets.js updated with the image files

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -6,12 +6,15 @@ import cart from './Cart.png';
 import cake_ingredients from './Cake_Ingredients.png';
 import cake_tools from './Cake_Tools.png';
 import party_items from './Party_Items.png';
-import dum_icing_sugar from './Dum_Icing_Sugar.png';
+import dum_glucose_syrup from './Dum_Glucose_Syrup.png';
 import test_img from './menu_4.png';
 import star_rate from './Rating.svg';
 import silicone_spatula from './silicone_spatula.png';
 import hbd_banner from './HBD_Banner.png';
-import header from './header.png';
+import dum_b_powder from './Dum_B_Powder.png'
+import silicone_mat from './Silicone_Mat.png';
+import gold_num_candle from './Number_Candle_Gold.png';
+import header from './header.svg';
 import add_icon_white from './add_icon_white.svg';
 import add_icon_green from './add_icon_green.svg';
 import remove_icon from './remove_icon_red.svg';
@@ -29,11 +32,14 @@ const assets = {
     cake_ingredients,
     cake_tools,     
     party_items,
-    dum_icing_sugar,
+    dum_glucose_syrup,
     test_img,
     star_rate,
     silicone_spatula,
     hbd_banner,
+    dum_b_powder,
+    silicone_mat,
+    gold_num_candle,
     header,
     add_icon_white,
     add_icon_green,
@@ -62,8 +68,8 @@ export const category_list = [
 export const item_list = [
     {
         _id: "1",
-        name: "Dum Icing Sugar",
-        image: dum_icing_sugar,
+        name: "Dum Glucose Syrup",
+        image: dum_glucose_syrup,
         price: 100,
         description: "",
         category: "Cake Ingredients",
@@ -81,6 +87,30 @@ export const item_list = [
         _id: "3",
         name: "Happy Birthday Banner",
         image: hbd_banner,
+        price: 100,
+        description: "",
+        category: "Party Items",
+    },
+    {
+        _id: "4",
+        name: "Dum Baking Powder",
+        image: dum_b_powder,
+        price: 100,
+        description: "",
+        category: "Cake Ingredients",
+    },
+    {
+        _id: "5",
+        name: "Silicone Mat",
+        image: silicone_mat,
+        price: 100,
+        description: "",
+        category: "Cake Tools",
+    },
+    {
+        _id: "6",
+        name: "Number Candle - Gold",
+        image: gold_num_candle,
         price: 100,
         description: "",
         category: "Party Items",
